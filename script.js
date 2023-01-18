30873d8e746900fa94a1852d2c3aaf9a7e4a131c
Paraskevas Leivadaros <paraskevasleivadaros@gmail.com>
2023-01-19 00:02:00 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme() {
-  const body = document.querySelector("body");
-  body.classList.toggle("dark");
-  body.classList.toggle("light");
-}
-
-// add a listener to the toggle button
-const toggleButton = document.querySelector("#toggle-theme-button");
-toggleButton.addEventListener("click", toggleTheme);
